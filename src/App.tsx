d9b141769f9fee7d40460e0e82303e9e960602de
Nathaniel Aquino <naquino14@outlook.com>
2023-08-29 12:16:22 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          ImHere is currently under construction.
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/ComputerScienceHouse/ImHere"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Project GitHub
+        </a>
+      </header>
+    </div>
+  )
+}
+
+export default App
